72b96d6b36b5f3771e42c22ec49aaadbc79daa0a
eonicle <eonicle@gmail.com>
2017-04-11 12:11:28 +0300

    testing entrypoint

--- 3.2/docker-entrypoint.sh
+++ 3.2/docker-entrypoint.sh
@@ -211,4 +211,16 @@ if [ "$originalArgOne" = 'mongod' ]; then
 	unset MONGO_INITDB_DATABASE
 fi
 
+MYIP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+IPS=$(arp-scan --interface=eth0 --localnet --numeric --quiet --ignoredups | grep -E '([a-f0-9]{2}:){5}[a-f0-9]{2}' | awk '{print $1}')
+for IP in $IPS
+do
+		IS_MASTER=$(mongo --host $IP --eval "printjson(db.isMaster())" | grep 'ismaster')
+    echo $IS_MASTER
+		if echo $IS_MASTER | grep "true"
+    then
+			mongo --host $IP --eval "printjson(rs.add('$MYIP:27017'))"
+		fi
+done
+
 exec "$@"
