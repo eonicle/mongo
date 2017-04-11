8a140e7d8f23f80ec9d1655549a789ff9f6269ac
eonicle <eonicle@gmail.com>
2017-04-11 12:26:40 +0300

    fixing root access problem

--- 3.2/docker-entrypoint.sh
+++ 3.2/docker-entrypoint.sh
@@ -212,7 +212,7 @@ if [ "$originalArgOne" = 'mongod' ]; then
 fi
 
 MYIP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-IPS=$(arp-scan --interface=eth0 --localnet --numeric --quiet --ignoredups | grep -E '([a-f0-9]{2}:){5}[a-f0-9]{2}' | awk '{print $1}')
+IPS=$(sudo arp-scan --interface=eth0 --localnet --numeric --quiet --ignoredups | grep -E '([a-f0-9]{2}:){5}[a-f0-9]{2}' | awk '{print $1}')
 for IP in $IPS
 do
 		IS_MASTER=$(mongo --host $IP --eval "printjson(db.isMaster())" | grep 'ismaster')
